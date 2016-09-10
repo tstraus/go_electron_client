e02671c533e7d9296b8c165bbe072e05f6405cbe
tstraus <tckrsstrsbgh@gmail.com>
2016-09-10 18:04:30 -0400

    added src directory

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# go_electron_client
+-to run
+electron .
+
+-for vscode node completion
+typings install dt~node --global
+typings install dt~socket.io-client --global
+and create a jsconfig.json using vscode green lightbulb
+
---
+++ src/main.js
@@ -0,0 +1,24 @@
+const { app, BrowserWindow } =  require('electron')
+
+let win
+
+function createWindow () {
+    win = new BrowserWindow({width: 600, height: 630})
+    win.loadURL('file://' + __dirname + '/index.html')
+
+    win.on('closed', () => {
+        win = null
+    })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+        app.quit()
+})
+
+app.on('activate', () => {
+    if (win === null) {
+        createWindow()
+    }
+})
